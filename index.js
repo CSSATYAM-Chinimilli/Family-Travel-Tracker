0eb18da4e1e2b409d2a11d401ed7d806a8bfa5c5
Satyam <chinimillichinasivasatyam@gmail.com>
2024-06-16 22:16:22 +0530

    Update index.js and index.ejs to handle errors

--- index.js
+++ index.js
@@ -19,69 +19,128 @@ app.use(express.static("public"));
 
 let currentUserId = 1;
 
-let users = [
-  { id: 1, name: "Angela", color: "teal" },
-  { id: 2, name: "Jack", color: "powderblue" },
-];
+let users = [];
 
-async function checkVisisted() {
-  const result = await db.query(
-    "SELECT country_code FROM visited_countries JOIN users ON users.id = user_id WHERE user_id = $1; ",
-    [currentUserId]
-  );
-  let countries = [];
-  result.rows.forEach((country) => {
-    countries.push(country.country_code);
-  });
-  return countries;
+async function checkVisited() {
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM visited_countries WHERE user_id = $1",
+      [currentUserId]
+    );
+    let countries = [];
+    result.rows.forEach((country) => {
+      countries.push(country.country_code);
+    });
+    console.log(countries);
+    return countries;
+  } catch (error) {
+    console.error(error.message);
+    console.log("No countries visited");
+  }
 }
 
 async function getCurrentUser() {
-  const result = await db.query("SELECT * FROM users");
-  users = result.rows;
-  return users.find((user) => user.id == currentUserId);
+  const user = await db.query("SELECT * FROM users");
+  users = user.rows;
+  try {
+    if (user.rows.length > 0) {
+      const result = await db.query(
+        "SELECT name, color FROM users WHERE id = $1",
+        [currentUserId]
+      );
+      return result.rows[0];
+    } else {
+      throw new Error("No users present");
+    }
+  } catch (error) {
+    console.log(error.message);
+    return false;
+  }
 }
 
 app.get("/", async (req, res) => {
-  const countries = await checkVisisted();
   const currentUser = await getCurrentUser();
+  const countries = await checkVisited();
+  if (currentUser == false) {
+    res.render("index.ejs", {
+      countries: countries,
+      users: users,
+      color: "teal",
+    });
+  } else {
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      users: users,
+      color: currentUser.color,
+      enterCountryBlock: true,
+    });
+  }
+});
+
+app.get("/homeRouteWithErrorMessage", async (req, res) => {
+  const error = req.query.error;
+  const currentUser = await getCurrentUser();
+  const countries = await checkVisited();
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
     users: users,
     color: currentUser.color,
+    error: error,
+    enterCountryBlock: true,
   });
 });
+
 app.post("/add", async (req, res) => {
-  const input = req.body["country"];
-  console.log(input);
-  const currentUser = await getCurrentUser();
+  const input = req.body.country;
 
   try {
+    // const result = await db.query(
+    //   "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+    //   [input.toLowerCase()]
+    // );
     const result = await db.query(
-      "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE  $1 || '%';",
+      "SELECT country_code FROM countries WHERE LOWER(country_name) = $1;",
       [input.toLowerCase()]
     );
-
-    const data = result.rows[0];
-    console.log(result);
-    const countryCode = data.country_code;
-    try {
-      await db.query(
-        "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
+    if (result.rows.length > 0) {
+      const countryCode = result.rows[0].country_code;
+      const isCountryPresent = await db.query(
+        "SELECT * FROM visited_countries WHERE country_code = $1 AND user_id = $2;",
         [countryCode, currentUserId]
       );
-      res.redirect("/");
-    } catch (err) {
-      console.log(err);
+      try {
+        if (isCountryPresent.rows.length == 0) {
+          await db.query(
+            "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2);",
+            [countryCode, currentUserId]
+          );
+          res.redirect("/");
+        } else {
+          throw new Error("Country already exists!");
+        }
+      } catch (err) {
+        const error = err.message;
+        console.log(err.message);
+        res.redirect(
+          `/homeRouteWithErrorMessage?error=${encodeURIComponent(error)}`
+        );
+      }
+    } else {
+      throw new Error("Invalid country name, please try again");
     }
   } catch (err) {
-    console.log(err);
+    const error = err.message;
+    console.log(err.message);
+    res.redirect(
+      `/homeRouteWithErrorMessage?error=${encodeURIComponent(error)}`
+    );
   }
 });
 
 app.post("/user", async (req, res) => {
-  if (req.body.add === "new") {
+  if (req.body.add == "new") {
     res.render("new.ejs");
   } else {
     currentUserId = req.body.user;
@@ -92,16 +151,26 @@ app.post("/user", async (req, res) => {
 app.post("/new", async (req, res) => {
   const name = req.body.name;
   const color = req.body.color;
+  const main1 = async () => {
+    res.render("edit.ejs", {
+      name: name,
+      errorMessage: "Please select a color",
+    });
+  };
 
-  const result = await db.query(
-    "INSERT INTO users (name, color) VALUES($1, $2) RETURNING *;",
-    [name, color]
-  );
+  if (!color) {
+    await main1();
+  } else {
+    console.log("both name and color fetched");
+    const newUser = await db.query(
+      "INSERT INTO users (name, color) VALUES ($1, $2) RETURNING *;",
+      [name, color]
+    );
 
-  const id = result.rows[0].id;
-  currentUserId = id;
-
-  res.redirect("/");
+    console.log("user data inserted in users table");
+    currentUserId = newUser.rows[0].id;
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
